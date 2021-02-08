7857f68eef427ccb66506f0ab9d5ae1b7b8070ef
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-08 17:55:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,4 +12,10 @@ import manageUsers from './reducers/manageUsers'
 let store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
