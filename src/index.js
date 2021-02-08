05f3b8031e02b62d603794581e1002de8dc7d89e
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-08 17:54:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 
-import manageUsers from './reducers/manaimport { Provider } from 'react-redux'
+
 import { createStore } from 'redux'
 import manageUsers from './reducers/manageUsers'
-geUsers'
+
 
 
 // add imports and code
