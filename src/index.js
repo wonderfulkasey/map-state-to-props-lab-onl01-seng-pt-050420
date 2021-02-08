a7d1ef1cfa8632c738d5cb9ce8ee4aae8ad9d1bd
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-08 17:55:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,12 +9,7 @@ import manageUsers from './reducers/manageUsers'
 
 
 // add imports and code
+let store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
-ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
-  ,
-  document.getElementById('root')
-);
+
